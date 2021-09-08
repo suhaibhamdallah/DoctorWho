a3e076fd389cd85e722385dc1bb9b27505cb0115
Suhaib Hamdallah <suhaib.hamdallah@gmail.com>
2021-09-08 23:29:14 +0300

    Add email sender services

---
+++ DoctorWho/DoctorWho.Notification/Services/EmailSenderService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Notification.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSenderService(IConfiguration configuration)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task SendEmailAsync(string toEmail, string emailSubject, string emailBody, bool isBodyHTML)
+        {
+            var fromMail = _configuration["EmailSenderService:SenderEmail"];
+            var fromPassword = _configuration["EmailSenderService:SenderEmailPassword"];
+            var senderDisplayName = _configuration["EmailSenderService:SenderDisplayName"];
+
+            var mailMessage = new MailMessage(fromMail, toEmail, emailSubject, emailBody)
+            {
+                From = new MailAddress(fromMail, senderDisplayName),
+                IsBodyHtml = isBodyHTML
+            };
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.SendAsync(mailMessage, null);
+        }
+    }
+}
